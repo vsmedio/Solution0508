cc7210a7a5312509c2bb851eabaec7ad741597c4
Arvis Abromovics <info@vsmedio.lv>
2018-01-06 22:26:38 +0100

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,53 @@
+/*
+Геттеры и сеттеры для класса Person
+Создать class Person. У человека должно быть имя String name, возраст int age, пол char sex.
+Создайте геттеры и сеттеры для всех переменных класса Person.
+
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. У класса Person должна быть переменная name с типом String.
+3. У класса Person должна быть переменная age с типом int.
+4. У класса Person должна быть переменная sex с типом char.
+5. У класса должен сеттер для переменной name.
+6. У класса должен геттер для переменной name.
+7. У класса должен сеттер для переменной age.
+8. У класса должен геттер для переменной age.
+9. У класса должен сеттер для переменной sex.
+10. У класса должен геттер для переменной sex.
+*/
+
+
+public class Person {
+    String name;
+    int age;
+    char sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
